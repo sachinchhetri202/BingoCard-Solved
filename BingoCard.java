91f9228a9f4788343076e4f924a3478bb97b3c8f
sachinchhetri202 <71232069+sachinchhetri202@users.noreply.github.com>
2021-04-06 14:54:22 -0600

    First commit

---
+++ BingoCard.java
@@ -0,0 +1,48 @@
+import java.util.*;
+
+public class BingoCard{
+
+    public static void main(String[] args){
+    
+        int[][] card = new int[5][5];
+        ArrayList<Integer> values = new ArrayList<Integer>();
+        boolean valid = false;
+        int tmp = 0;  // stores the random number
+        
+        // 5 * 5 this iteraters 0 to 24 times
+        for(int i = 0; i <= 4; i++){
+            for(int row = 0; row < card.length; row++){
+                while(!valid){ // checks if it is not valid
+                    tmp = (int)(Math.random() * 15) + 1 + 15 * i; // after each iteration the number increases 
+                    if(!values.contains(tmp)){ // if the value is not already in the Arraylist of values then it stores
+                        valid = true; // if new number then it adds in the array
+                        values.add(tmp);
+                    }
+                } // end while loop
+                card[row][i] = tmp;
+                valid = false;
+            } // end inner loop
+        } // end outer loop
+        card[2][2] = 0;
+        
+        
+        //creating an array for the title.  
+        String title[]= {"B","I","N","G","O"};
+    
+        for(int i = 0; i < title.length; i++){
+            System.out.print(title[i]+ "\t");
+        }
+        System.out.println();
+        
+        // r = row (horizontal)
+        // c = column (vertical)
+        for(int r = 0; r < card.length; r++){
+            for(int c = 0; c < card[r].length; c++){
+                System.out.print(card[r][c]+ "\t");
+            }
+            System.out.println();
+        }
+    }
+
+}
+
